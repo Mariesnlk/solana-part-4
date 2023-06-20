705a0cf7d25dc9196749dc79a1a122411c3472cd
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-06-20 22:14:35 +0200

    added comment on review functionality

--- solana-movie-program/src/state.rs
+++ solana-movie-program/src/state.rs
@@ -1,11 +1,9 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    program_pack::{IsInitialized, Sealed},
-    pubkey::Pubkey,
-};
+use borsh::{ BorshDeserialize, BorshSerialize };
+use solana_program::{ program_pack::{ IsInitialized, Sealed }, pubkey::{ Pubkey, self } };
 
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct MovieAccountState {
+    pub discriminator: String,
     pub is_initialized: bool,
     pub reviewer: Pubkey,
     pub rating: u8,
@@ -13,9 +11,66 @@ pub struct MovieAccountState {
     pub title: String,
 }
 
+impl MovieAccountState {
+    pub const DISCRIMINATOR: &'static str = "review";
+
+    pub fn get_account_size(title: String, description: String) -> usize {
+        return (
+            4 +
+            MovieAccountState::DISCRIMINATOR.len() +
+            1 +
+            1 +
+            (4 + title.len()) +
+            (4 + description.len())
+        );
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieCommentCounter {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub counter: u64,
+}
+
+impl MovieCommentCounter {
+    pub const DISCRIMINATOR: &'static str = "counter";
+    pub const SIZE: usize = 4 + MovieCommentCounter::DISCRIMINATOR.len() + 1 + 8;
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieComment {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub review: Pubkey,
+    pub commenter: Pubkey,
+    pub comment: String,
+    pub counter: u64,
+}
+
+impl MovieComment {
+    pub const DISCRIMINATOR: &'static str = "comment";
+
+    pub fn get_account_size(comment: String) -> usize {
+        return 4 + MovieComment::DISCRIMINATOR.len() + 1 + 32 + 32 + (4 + comment.len()) + 8;
+    }
+}
+
 impl Sealed for MovieAccountState {}
 
 impl IsInitialized for MovieAccountState {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl IsInitialized for MovieCommentCounter {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl IsInitialized for MovieComment {
     fn is_initialized(&self) -> bool {
         self.is_initialized
     }
